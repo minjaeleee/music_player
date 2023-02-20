ba5ba777cae3854d1505c409a9149371a45be5fb
minjaeleee <dlalswo4602@naver.com>
2023-02-20 21:48:42 +0900

    드래그 정렬 기능 완성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import SortableList from './lib/SortableList'
+import TestItem from './TestItem/TestItem'
+import { data } from './TestItem/TestData'
 
 function App() {
+  const onClickItem = (index) => alert(index)
+  const onDropItem = newList => console.log(newList)
+  const renderItem = (item, index) => <TestItem data={item} index={index} />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <SortableList
+      data={data}
+      // renderItem={(item, index) => <TestItem data={item} index={index} />}
+      renderItem={renderItem}
+      onDropItem={onDropItem}
+      onClickItem={onClickItem}
+    />
+  )
 }
 
 export default App;
